7f186ca098ce0ef1570473a539529048bae3e8e8
Purav Nisar <36162246+Puravnisar@users.noreply.github.com>
2020-08-11 18:59:13 -0700

    added business tab

---
+++ business.js
@@ -0,0 +1,15 @@
+//Purav Nisar
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Business extends React.Component{
+	render()
+	{
+		return(<p>In Business</p>)
+	};
+}
+
+export default Business;
